d2f05bea2900313c22453d8ae2265a47c21b31d6
GuiSantosMacedo1 <guisantosmacedo7@gmail.com>
2025-10-07 14:35:21 -0300

    feat: Implementação do menu dinâmico na dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,18 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from "@angular/router";
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './dashboard.component.html',
+  imports: [CommonModule, RouterLink],
+templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    this.router.events
+    .pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe((event: NavigationEnd) => {
+      this.updateActiveMenuFromRoute(event.url);
+    });
   }
 
+  navigateTo(route: string, itemId: string): void {
+    this.setActiveTab(itemId)
+    this.router.navigate([route]);
+  }
+
+  private updateActiveMenuFromRoute(url: string): void {
+    const menuItem = this.menuItems.find(item => 
+      url.includes(item.route.substring(1)));
+    if (menuItem) {
+      this.activeTab = menuItem.tab;
+    }
+  }
+
+  activeTab: string = 'dashboard';
+
+  menuItems = [
+    { name: 'Dashboard', tab: 'dashboard', route: '/dashboard' },
+    { name: 'Transactions', tab: 'transactions', route: '/transactions' },
+    { name: 'Budgets', tab: 'budgets', route: '/budgets' },
+    { name: 'Metas', tab: 'metas', route: '/metas' },
+    { name: 'Settings', tab: 'settings', route: '/settings' }
+  ]
+
+  setActiveTab(tab: string): void {
+    this.activeTab = tab;
+  }
+  isActiveTab(tab: string): boolean {
+    return this.activeTab === tab;
+  }
 }
