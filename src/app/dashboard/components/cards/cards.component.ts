f5cae4332c22517f16f835fc061a0587c6747e2b
GuiSantosMacedo1 <guisantosmacedo7@gmail.com>
2025-10-09 18:18:56 -0300

    feat: implement lógica de transações com cálculo de receitas, despesas e saldo

--- src/app/dashboard/components/cards/cards.component.ts
+++ src/app/dashboard/components/cards/cards.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-
+import { TransactionsService } from '../../services/transactions.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-cards',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.scss'
 })
 export class CardsComponent {
-  
+
+  transactions: any[] = [];
+
+  constructor(private transactionsService: TransactionsService) { }
+  ngOnInit(): void {
+    this.transactions = this.transactionsService.getTransactions();
+  }
+
+  calculateTotalIncome(): number {
+    return this.transactions
+      .filter(gasto => gasto.amount > 0).reduce((sum, gasto) => sum + gasto.amount, 0);
+  }
+  calculateTotalExpense(): number {
+    return this.transactions
+    .filter(gasto => gasto.amount < 0).reduce((sum, gasto) => sum + gasto.amount, 0);
+  }
+  calculateBalance(): number {
+    return this.calculateTotalIncome() + this.calculateTotalExpense();
+  }
 }
