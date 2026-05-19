b79e0bb6d606f47ac94184daa4cc3facaf5b4643
GuiSantosMacedo1 <guisantosmacedo7@gmail.com>
2026-05-18 22:41:17 -0300

    feat: move Transactions component to shared folder and update references

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@ import { Component, EventEmitter, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, NavigationEnd } from '@angular/router';
 import { CardsComponent } from './components/cards/cards.component';
-import { TransactionsComponent } from "./components/transactions/transactions.component";
+import { TransactionsComponent } from "../shared/transactions/transactions.component";
 import { ModalTransactionsComponent } from '../shared/modal-transactions/modal-transactions.component';
 import { SideBar } from "../core/layout/sidebar/side-bar.component";
 
--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SideBar } from "../core/layout/sidebar/side-bar.component";
 import { ModalTransactionsComponent } from "../shared/modal-transactions/modal-transactions.component";
+import { NgFor } from '@angular/common';
+import { TransactionsService } from '../core/services/transactions.service';
 
 @Component({
   selector: 'app-transactions',
   standalone: true,
-  imports: [SideBar, ModalTransactionsComponent],
+  imports: [SideBar, ModalTransactionsComponent, NgFor],
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.scss'
 })
 export class TransactionsComponent {
   activeModal = false
+
+  transactions: any[] = [];
+  categories: string[] = [];
+
+  constructor(private transactionsService: TransactionsService) {}
+
+  ngOnInit(): void {
+    this.transactionsService.getTransactions().subscribe((response: any) => {
+      this.transactions = response.data || [];
+      this.categories = [...new Set(this.transactions.map(transaction => transaction.category).filter(Boolean))];
+    });
+  }
 }
