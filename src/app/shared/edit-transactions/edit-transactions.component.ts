ebb4cdad1f6b607ddfa94a4af12e7282bbc1d00e
GuiSantosMacedo1 <guisantosmacedo7@gmail.com>
2026-05-20 17:42:06 -0300

    feat: adiciona máscara de moeda e validação no formulário de edição de transações

--- src/app/shared/edit-transactions/edit-transactions.component.ts
+++ src/app/shared/edit-transactions/edit-transactions.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TransactionsService } from '../../core/services/transactions.service';
+import { CurrencyMaskModule } from 'ng2-currency-mask';
 
 @Component({
   selector: 'app-edit-transactions',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, CurrencyMaskModule],
   templateUrl: './edit-transactions.component.html',
   styleUrl: './edit-transactions.component.scss'
 })
@@ -16,10 +17,11 @@ export class EditTransactionsComponent {
   @Output() saved = new EventEmitter<void>()
   @Output() closed = new EventEmitter<void>()
 
+  errorMessage:any = null
   localTransaction: any = {
     _id: '',
     description: '',
-    amount: 0,
+    amount: '',
     date: '',
     type: 'income',
     category: ''
@@ -31,39 +33,41 @@ export class EditTransactionsComponent {
     if(changes['transaction'] && this.transaction) {
       this.localTransaction = { ...this.transaction }
     }
-    if(changes['isOpen'] && !this.isOpen) {
-      this.localTransaction = {
-        _id: '',
-        description: '',
-        amount: 0,
-        date: '',
-        type: 'income',
-        category: ''
-      }
-    }
   }
   
   closeModal() {
     this.closed.emit()
   }
-  
+
+    validateForm(): boolean {
+    if (!this.localTransaction.description || this.localTransaction.amount <= 0 || !this.localTransaction.type) {
+      return false;
+    }
+    return true;
+  }
+
   editTransactions(){
-    if(!this.transaction?._id) return;
-    
+    if(!this.localTransaction?._id){
+      this.errorMessage = 'Erro ao encontrar o ID'
+      return
+    }
+    if(!this.validateForm()){
+      this.errorMessage = 'Precisa preencher todos os campos'
+      return
+    }
     const payload = {
-      category: this.transaction.category,
-      description: this.transaction.description,
-      amount: this.transaction.amount,
-      date: this.transaction.date,
-      type: this.transaction.type
+      category: this.localTransaction.category,
+      description: this.localTransaction.description,
+      amount: this.localTransaction.amount,
+      date: this.localTransaction.date,
+      type: this.localTransaction.type
     };
     
-    this.transactionsService.putTransactions(this.transaction._id, payload).subscribe({
+    this.transactionsService.putTransactions(this.localTransaction._id, payload).subscribe({
       next: () => {
         this.saved.emit();
         this.closeModal();
-      }
+      } 
     })
-    console.log("🚀 ~ EditTransactionsComponent ~ editTransactions ~ editTransactions:", this.editTransactions)
   }
 }
