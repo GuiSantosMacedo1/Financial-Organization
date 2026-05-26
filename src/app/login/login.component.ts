7a0a4e64607a6f815e128e612c98ceed1612508f
GuiSantosMacedo1 <guisantosmacedo7@gmail.com>
2026-05-25 23:04:30 -0300

    feat: criação de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { LoginService } from '../core/services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  
+  @Output() userCreate = new EventEmitter<any>()
+  constructor(private loginService: LoginService, private route: Router){}
+
+  errorMessage = ''
+  login = {
+    email: '',
+    password: ''
+  }
+
+  createUser() {
+    if(!this.validateForm()){
+      this.errorMessage = 'Por favor, preencha todos os campos corretamente.';
+      return;
+    }
+    this.errorMessage = '';
+
+    this.loginService.loginUser(this.login).subscribe(response => {
+      this.userCreate.emit(response);
+      this.route.navigate(['/dashboard'])
+    }, error => {
+      this.errorMessage = 'Erro ao logar'
+      console.error('Erro ao logar', error)
+    })
+    
+  }
+
+    validateForm(): boolean {
+    if (!this.login.email || !this.login.password) {
+      return false;
+    }
+    return true;
+  }
 }
