fad49533ae4d4f2678765987bd2ab13c080b0050
GuiSantosMacedo1 <guisantosmacedo7@gmail.com>
2026-06-06 20:39:17 -0300

    feat: adicionando o skeleton no recent transactions na tela Transactions

--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DestroyRef, inject } from '@angular/core';
 import { SideBar } from "../core/layout/sidebar/side-bar.component";
 import { ModalTransactionsComponent } from "../shared/modal-transactions/modal-transactions.component";
 import { NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TransactionsService } from '../core/services/transactions.service';
+import { finalize } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { CardsComponent } from "../shared/cards/cards.component";
 import { RecentTransactionsComponent } from '../shared/recent-transactions/recent-transactions.component';
 
@@ -20,24 +22,36 @@ export class TransactionsComponent {
   searchTerm = '';
   searchCategory = '';
   searchType = '';
-
+  isLoading = true
   transactions: any[] = [];
   categories: string[] = [];
-  transactionsChangedSubscription: any = null
+  private destroyRef = inject(DestroyRef);
 
   constructor(private transactionsService: TransactionsService) {}
 
   ngOnInit(): void {
-    this.loadTransaction();
-    this.transactionsChangedSubscription = this.transactionsService.transactionsChanged$.subscribe(() => {
-      this.loadTransaction();
-    });
+    this.loadTransaction(true);
+    this.transactionsService.transactionsChanged$
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(() => {
+        this.loadTransaction(false);
+      });
   }
-  loadTransaction(){
-    this.transactionsService.getTransactions().subscribe((response: any) => {
-      this.transactions = response.data || [];
-      this.categories = [...new Set(this.transactions.map(transaction => transaction.category).filter(Boolean))];
-    });
+  loadTransaction(showSkeleton = true){
+    if (showSkeleton) {
+      this.isLoading = true;
+    }
+    this.transactionsService.getTransactions()
+      .pipe(finalize(() => { this.isLoading = false; }))
+      .subscribe({
+        next: (response: any) => {
+          this.transactions = response.data || [];
+          this.categories = [...new Set(this.transactions.map(transaction => transaction.category).filter(Boolean))];
+        },
+        error: () => {
+          this.transactions = [];
+        }
+      });
   }
 
   get filteredTransactions(): any[] {
